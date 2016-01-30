f537e343342380bc10a0848ad624c48e270c5cd0
John Serrano <johnserrano404@gmail.com>
2016-01-29 17:32:00 -0800

    added optional scripts

---
+++ hello_world/4-hello_asm.asm
@@ -0,0 +1,19 @@
+section		.data
+
+msg:    db "Hello, asm",0xa
+len     equ $-msg
+
+section         .text
+global          main
+
+main:
+	mov edx, len
+	mov ecx, msg
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
+
