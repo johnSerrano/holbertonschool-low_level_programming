8906deb5de833d549e3036037ef3a99c5960143c
John Serrano <johnserrano404@gmail.com>
2016-02-25 10:40:06 -0800

    triangles

---
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -0,0 +1,17 @@
+int print_char(char);
+
+void triangles_in_the_term(int h , int n)
+{
+	int i, space, height, width;
+	for (i = 0; i < n; i++) {
+		for (height = 0; height < h; height ++) {
+			for (space = 0; space < (h - height); space++) {
+				print_char(' ');
+			}
+			for (width = 0; width < (height * 2) + 1; width++) {
+				print_char('*');
+			}
+		print_char('\n');
+		}
+	}
+}
---
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/main.c
@@ -0,0 +1,7 @@
+void triangles_in_the_term(int, int);
+
+int main()
+{
+	triangles_in_the_term(4, 1);
+	return(0);
+}
