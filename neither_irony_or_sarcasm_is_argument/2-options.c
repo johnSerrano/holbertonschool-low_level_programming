c2c8b9cf8b7bd03d7458c20f09e61e00710d54bd
John Serrano <johnserrano404@gmail.com>
2016-04-04 13:45:28 -0700

    made optional for irony

--- neither_irony_or_sarcasm_is_argument/2-options.c
+++ neither_irony_or_sarcasm_is_argument/2-options.c
@@ -2,25 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int print_as_single_args(char *, int);
-int used(char **, char *);
-
-/*TODO check used_args*/
+int print_as_single_args(char *, int, char **, int);
+int check_string_args(int, char **, char *);
 
 int main(int argc, char **argv)
 {
-  char **used_args;
   int i;
   int first = 1;
   char *argptr;
-  used_args = malloc(sizeof(char *));
-  used_args[0] = "";
   for (i=1; i<argc; i++) {
-    if (strcmp(argv[i], "--") == 0) break;
     argptr = argv[i];
-    if (used(argv, argptr) == 1) continue;
+    if (strcmp(argv[i], "--") == 0) break;
     if (argv[i][0] == '-') {
       if (argv[i][1] == '-') {
+        if (check_string_args(argc, argv, argptr) == 1) continue;
         if (first != 1)
           printf(", ");
         else
@@ -33,7 +28,7 @@ int main(int argc, char **argv)
       else {
         argptr = argv[i];
         argptr++;
-        first = print_as_single_args(argptr, first);
+        first = print_as_single_args(argptr, first, argv, argc);
       }
     }
   }
@@ -41,18 +36,58 @@ int main(int argc, char **argv)
   return 0;
 }
 
-int used(char **argv, char *arg) {
-  argv++;
-  arg++;
+int check_string_args(int argc, char **argv, char *to_check)
+{
+  int total = 0;
+  int i;
+  char *argptr;
+  for (i=1; i<argc; i++) {
+    argptr = argv[i];
+    if (argptr == to_check) break;
+    if (strcmp(argv[i], "--") == 0) break;
+    if (argv[i][0] == '-') {
+      if (argv[i][1] == '-') {
+        if (strcmp(argptr, to_check) == 0) return 1;
+      }
+      else {
+        /*pass*/
+      }
+    }
+  }
   return 0;
 }
 
-int print_as_single_args(char *s, int first) {
+int check_single_args(int argc, char **argv, char *to_check)
+{
+  int i;
+  char *argptr;
+  for (i=1; i<argc; i++) {
+    argptr = argv[i];
+    if (argptr == to_check) break;
+    if (strcmp(argv[i], "--") == 0) break;
+    if (argv[i][0] == '-') {
+      if (argv[i][1] != '-')
+        while (*argptr != 0) {
+          if (argptr == to_check) return 0;
+          if(*to_check == *argptr) return 1;
+          argptr++;
+      }
+    }
+  }
+  return 0;
+}
+
+int print_as_single_args(char *s, int first, char **argv, int argc) {
   while (*s != 0) {
+    if (check_single_args(argc, argv, s) != 0) {
+      s++;
+      continue;
+    }
     if (first != 1)
       printf(", ");
     else
       first = 0;
+
     printf("%c", *s);
     s++;
   }
