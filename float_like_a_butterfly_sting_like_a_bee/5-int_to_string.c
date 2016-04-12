facf40e3c671319b0a8c76555d7e29b4df18fba6
John Serrano <johnserrano404@gmail.com>
2016-04-12 09:14:39 -0700

    malloc project problem 5

--- float_like_a_butterfly_sting_like_a_bee/5-int_to_string.c
+++ float_like_a_butterfly_sting_like_a_bee/5-int_to_string.c
@@ -1,20 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-char *do_work(int, char**);
+char *do_work(int, char*);
 
-char  *add_digit(char digit, char **str) {
+char  *add_digit(char digit, char *str) {
   int counter = 2; /* nonzero because null terminator and extension */
-  char *strc = *str;
-  while (**str != 0) {
+  char *strc = str;
+  while (*str != 0) {
     counter++;
-    (*strc)++;
+    str++;
   }
-  *(*(str)+counter-2) = digit;
-  *str = realloc(*str, sizeof(char)*(counter));
-/*  (*str)[counter-1] = 0; */
-  printf("STRING: %s\n", *str);
-  return *str;
+  *(strc+counter-2) = digit;
+  str = malloc(sizeof(char)*(counter));
+  str = strcpy(str, strc);
+  return str;
 }
 
 char *int_to_string(int n)
@@ -23,33 +23,35 @@ char *int_to_string(int n)
   *str = 0;
 
 	if (n < 0) {
-    str = add_digit('-', &str);
-		n = 0-n;
-	}
+    str = add_digit('-', str);
+	} else {
+    n = 0-n;
+  }
 	if (n == 0) {
-    str = add_digit('0', &str);
+    str = add_digit('0', str);
 	}
 	else {
-		str = do_work(n, &str);
+		str = do_work(n, str);
 	}
   return str;
 }
 
-char *do_work(int n, char **str)
+char *do_work(int n, char *str)
 {
 	int divisor = 1;
 	int a = n;
-	while (a > 9) {
+	while (a < -9) {
 		a /= 10;
 		divisor *= 10;
 	}
 
-	while (n > 0) {
+	while (n < 0) {
 		int to_print = n / divisor;
 		n -= to_print * divisor;
 		divisor /= 10;
-	  *str = add_digit('0' + to_print, str);
+    to_print = 0-to_print;
+	  str = add_digit('0' + to_print, str);
 
 	}
-  return *str;
+  return str;
 }
