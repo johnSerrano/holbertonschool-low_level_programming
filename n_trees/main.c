76152da4eb804da156707326799beb535531b73a
John Serrano <johnserrano404@gmail.com>
2016-07-29 13:17:16 -0700

    wrote a test file. maybe it works.

--- n_trees/main.c
+++ n_trees/main.c
@@ -5,12 +5,23 @@
 int ntree_insert(NTree **tree, char **parents, char *data);
 void print_ntree(NTree *);
 void print_tree_recurse(NTree *tree, int tabs);
+int get_words(char *str, char seperator);
+int get_word_len(char *fast, char seperator);
+char **string_split(char *ptr, char seperator);
 
 int main() {
   /*test insert*/
   NTree *tree = NULL;
   ntree_insert(&tree, NULL, "/");
 
+  ntree_insert(&tree, string_split("/", ' '), "home");
+  // ntree_insert(&tree, string_split("/", ' '), "opt");
+  ntree_insert(&tree, string_split("/ home", ' '), "john");
+  // ntree_insert(&tree, string_split("/ home", ' '), "sys");
+  // ntree_insert(&tree, string_split("/ home john", ' '), "other");
+  // ntree_insert(&tree, string_split("/ opt", ' '), "bin");
+  print_ntree(tree);
+  return 0;
 }
 
 void print_ntree(NTree *tree)
@@ -34,9 +45,83 @@ void print_tree_recurse(NTree *tree, int tabs)
   }
   if (tree->str == NULL){
     printf("NULL\n");
+  } else {
+    printf("%s\n", tree->str);
   }
-  printf("%s\n", tree->str);
   for (ptr = tree->children; ptr != NULL; ptr++) {
+    // printf("debugging pointer check: %p\n", ptr);
+    // printf("debugging node check: %p\n", ptr->node);
+    if (ptr->node != NULL)
+      // printf("debugging str check: %s\n", ptr->node->str);
+
     print_tree_recurse(ptr->node, tabs+1);
   }
+}
+
+/*
+ * Function that splits strings into array of strings depending on
+ * seperator.
+ */
+char **string_split(char *ptr, char seperator)
+{
+	char **ret;
+	char *ret_ptr;
+	int count = 0;
+	int word_count = 0;
+	count = get_words(ptr, seperator);
+	if (count == 0) {
+		ret = malloc(sizeof(char *)*1);
+		ret[0] = NULL;
+		return ret;
+	}
+	ret = malloc(sizeof(char *) * (count+2));
+	while (*ptr != 0) {
+		while (*ptr == seperator)
+			ptr++;
+		count = get_word_len(ptr, seperator);
+		ret[word_count] = malloc(sizeof(char) * (count + 1));
+		ret_ptr = ret[word_count];
+		while (*ptr != seperator && *ptr != 0) {
+			*ret_ptr = *ptr;
+			ret_ptr++;
+			ptr++;
+		}
+		while (*ptr == seperator)
+			ptr++;
+		*ret_ptr = 0;
+		word_count++;
+	}
+	ret[word_count] = NULL;
+	return ret;
+}
+
+/*
+ * gets the number of segments to break the string into
+ */
+int get_words(char *ptr, char seperator)
+{
+	int count = 0;
+	while (*ptr != 0) {
+		while (*ptr == seperator)
+			ptr++;
+		if (*ptr == 0)
+			break;
+		while (*ptr != seperator && *ptr != 0)
+			ptr++;
+		count++;
+	}
+	return count;
+}
+
+/*
+ * gets the length of the segment starting at fast
+ */
+int get_word_len(char *fast, char seperator)
+{
+	int count = 0;
+	while (*fast != seperator && *fast != 0) {
+		count++;
+		fast++;
+	}
+	return count;
 }
--- n_trees/ntree_insert.c
+++ n_trees/ntree_insert.c
@@ -4,20 +4,41 @@
 
 int ntree_insert(NTree **tree, char **parents, char *data)
 {
-  List *ptr = (*tree)->children;
+  List **ptr;
+  List *tmp;
+  int i;
   if (*tree == NULL) {
     /* create tree */
+    *tree = malloc(sizeof(NTree));
+    if (*tree == NULL) return 1;
     /* add data as root node */
+    (*tree)->str = strdup(data);
+    (*tree)->children = NULL;
+    return 0;
   } else {
+    ptr = &((*tree)->children);
+
     /* if parents[0] or parents is null and there is already a root,
        we have a problem */
-    int i;
     for (i=0 ; parents[i] != NULL ; i++)
       /* if parents[0] is null add data as node */
       /* for node in ntrees list*/
-      while (strcmp(ptr->node->str, data) != 0 || ptr != NULL) {
-        ptr = ptr->next;
+      if (parents[i+1] == NULL) {
+        /*insert here*/
+        tmp = *ptr;
+        *ptr = malloc(sizeof(List));
+        (*ptr)->next = tmp;
+        (*ptr)->node = malloc(sizeof(NTree));
+        (*ptr)->node->str = strdup(data);
+        (*ptr)->node->children = NULL;
+        return 0;
+      } else {
+        /* find child where str == parents[i]*/
+        while ((*ptr)->next != NULL && strcmp((*ptr)->node->str, parents[i]) != 0) {
+          *ptr = (*ptr)->next;
+        }
+        *ptr = (*ptr)->node->children;
       }
-      ptr->node->str = strdup(data);
-  }
+    }
+  return 0;
 }
